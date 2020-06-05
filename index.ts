913ee57f7fe9b75121448091f188d13670a9cf0a
Rotimi Babalola <tolurotimi@hotmail.com>
2020-06-05 11:38:45 +0200

    Move location of index file

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from './src/components/Button';
+export { default as Form } from './src/components/Form';
+export { default as Input } from './src/components/Input';
+export { default as Label } from './src/components/Label';
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './components/Button';
-export { default as Form } from './components/Form';
-export { default as Input } from './components/Input';
-export { default as Label } from './components/Label';
