fa7218bafb35ca4c3a06bb3ac4bfc1211d06de58
Rotimi Babalola <tolurotimi@hotmail.com>
2020-06-05 09:02:30 +0200

    Publish package to github

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from './components/Button';
+export { default as Form } from './components/Form';
+export { default as Input } from './components/Input';
+export { default as Label } from './components/Label';
