73f5e03d0c55dae3cca4d024e3ff97043f5c894c
Rotimi Babalola <tolurotimi@hotmail.com>
2020-06-07 12:39:23 +0200

    Modify main file in package.json

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Button_1 = require("./components/Button");
+var Button_1 = require("./src/components/Button");
 Object.defineProperty(exports, "Button", { enumerable: true, get: function () { return Button_1.default; } });
-var Form_1 = require("./components/Form");
+var Form_1 = require("./src/components/Form");
 Object.defineProperty(exports, "Form", { enumerable: true, get: function () { return Form_1.default; } });
-var Input_1 = require("./components/Input");
+var Input_1 = require("./src/components/Input");
 Object.defineProperty(exports, "Input", { enumerable: true, get: function () { return Input_1.default; } });
-var Label_1 = require("./components/Label");
+var Label_1 = require("./src/components/Label");
 Object.defineProperty(exports, "Label", { enumerable: true, get: function () { return Label_1.default; } });
 //# sourceMappingURL=index.js.map
